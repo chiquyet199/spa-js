ce2fd0877477eae41578681c29d6e23d2280224c
Quyet <quyet@lingio.co>
2018-12-14 15:52:29 +0700

    Make shopping cart working

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,9 +1,10 @@
+import store from '../js/store.js'
 import {$} from '../js/utils.js'
-import {goback} from '../js/navigation.js'
+import {goback, navigate} from '../js/navigation.js'
 import { get, hosts } from "../js/api.js";
 
 const renderProductItem = product =>
-  `<li data-id="${product.id}">${
+  `<li class="product-item" data-id="${product.id}">${
     product.name
   }</li>`;
 
@@ -20,21 +21,28 @@ const render = (rootEl, props) => {
     </div>
   `;
   rootEl.innerHTML = template;
-  const backButton = $('.product .btn-back')
-  backButton.onclick = () => goback()
+  const productEl = $('.product')
+  productEl.addEventListener('click', function(event){
+    const targetElClassNames = event.target.classList
+    if(targetElClassNames.contains('product-item')){
+      funcs.openProductDetail(event.target.getAttribute('data-id'))
+    }else if(targetElClassNames.contains('btn-back')){
+      goback()
+    }
+  })
 };
 
 const funcs = {
   openProductDetail: function(id) {
     const appState = store.getState();
     const { products } = appState;
-    const activeProduct = products.find(p => p.id === id);
+    const activeProduct = products.find(p => p.id == id);
     const newState = {
       ...appState,
-      activePage: "productsDetail",
       activeProduct: activeProduct
     };
     store.setState(newState);
+    navigate('productsDetail')
   },
   fetchProducts: async function() {
     var products = await get(`${hosts.nordic}products`);
--- src/pages/productsDetail.js
+++ src/pages/productsDetail.js
@@ -1,3 +1,7 @@
+import store from '../js/store.js'
+import {$} from '../js/utils.js'
+import {navigate} from '../js/navigation.js'
+
 const render = (rootEl, props) => {
   const { activeProduct } = props;
   const template = activeProduct ? `
@@ -7,26 +11,30 @@ const render = (rootEl, props) => {
         <li>Name:${activeProduct.name}</li>
         <li>Prize:${activeProduct.prize}</li>
       </ul>
-      <button onclick="spa_product_detail.goTo('home')">Back To Home</button>
-      <button onclick="spa_product_detail.addToCart()">Add To Cart</button>
+      <button class="btn-back">Back To Home</button>
+      <button class="add-to-cart">Add To Cart</button>
     </div>
-  ` : '<h1>No such a product</h1>';
+  ` : '<h1>No such a product</h1>'
   rootEl.innerHTML = template;
+  const productEl = $('.product-detail')
+  productEl.addEventListener('click', function (event) {
+    const targetElClassNames = event.target.classList
+    if (targetElClassNames.contains('add-to-cart')) {
+      funcs.addToCart()
+    } else if (targetElClassNames.contains('btn-back')) {
+      navigate('home')
+    }
+  })
 };
 
-// const funcs = {
-//   goTo: function(page) {
-//     const appState = store.getState();
-//     const newState = { ...appState, activePage: page };
-//     store.setState(newState);
-//   },
-//   addToCart: function() {
-//     const appState = store.getState();
-//     const { activeProduct } = appState;
-//     const newState = { ...appState, cart: [...appState.cart, activeProduct] };
-//     store.setState(newState);
-//   }
-// };
+const funcs = {
+  addToCart: function() {
+    const appState = store.getState();
+    const { activeProduct } = appState;
+    const newState = { ...appState, cart: [...appState.cart, activeProduct] };
+    store.setState(newState);
+  }
+};
 
 export default {
   render
