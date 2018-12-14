189bce857091b1e45ff339b7b3a369609b2b83bf
Quyet <quyet@lingio.co>
2018-12-14 14:08:14 +0700

    Hook back button on browser

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 import store from "./store.js";
+import {hookBrowserBackButton} from "./navigation.js";
 import {getActivePage} from "./utils.js";
 
 const run = () => {
+  hookBrowserBackButton()
   const rootEl = document.getElementById('root')
   const appState = store.getState()
   const activePage = getActivePage();
--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,5 +1,7 @@
 import store from "./store.js";
 
+let listener = null
+
 export const navigate = page => {
   const appState = store.getState();
   const newState = {
@@ -12,9 +14,25 @@ export const navigate = page => {
 
 export const goback = () => {
   const appState = store.getState();
-  appState.history.pop();
+  appState.history.length > 1 && appState.history.pop()
   const prevPage = appState.history[appState.history.length - 1]
   if (!prevPage) return;
   const newState = { ...appState, activePage: prevPage };
   store.setState(newState);
 };
+
+export function hookBrowserBackButton() {
+  if (listener) {
+    unhook()
+  }
+  listener = event => {
+    console.log('on popstate')
+    goback()
+  }
+  window.addEventListener('popstate', listener)
+}
+
+export function unhook() {
+  window.removeEventListener('popstate', listener)
+  listener = null
+}
