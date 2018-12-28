0dd3c4d4ad28e480fa4a68587b09c209bba81de5
Quyết Nguyễn <quyet@lingio.co>
2018-12-28 14:32:27 +0700

    Remove old app

--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import home from "./home.js";
-import contact from "./contact.js";
-import products from "./products.js";
-import checkout from "./checkout.js";
-
-export default { home, contact, products, checkout };
